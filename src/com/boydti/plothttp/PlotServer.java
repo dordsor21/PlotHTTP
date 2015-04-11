a9f69e3bf5a803701ecfd2ffb36ff674e202e7eb
boy0001 <jessepaleg@gmail.com>
2015-04-11 19:10:46 +1000

    Added IP filtering

--- src/com/boydti/plothttp/PlotServer.java
+++ src/com/boydti/plothttp/PlotServer.java
@@ -1,6 +1,9 @@
 package com.boydti.plothttp;
 
 import java.util.Map;
+import java.util.Map.Entry;
+
+import com.boydti.plothttp.NanoHTTPD.Response.Status;
 
 public class PlotServer extends NanoHTTPD {
     public PlotServer() {
@@ -8,9 +11,32 @@ public class PlotServer extends NanoHTTPD {
     }
     
     @Override public Response serve(IHTTPSession session) {
+        Map<String, String> headers = session.getHeaders();
+        String ip = headers.get("remote-addr");
+        
+        if (Main.whitelist) {
+            if (!Main.allowedIps.contains(ip)) {
+                System.out.print("Denied query from: " + ip);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+                return new NanoHTTPD.Response(Status.FORBIDDEN, MIME_PLAINTEXT, "403 FORBIDDEN");
+            }
+        }
+        
+        
         Method method = session.getMethod();
         String uri = session.getUri();
-        Map<String, String> parms = session.getParms();
+        Map<String, String> params = session.getParms();
+        
+        System.out.print("IP: " + ip);
+        System.out.print("METHOD: " + method.name());
+        System.out.print("URI: " + uri);
+        System.out.print("PARAMS:");
+        for (Entry<String, String> entry : params.entrySet()) {
+            System.out.print(" - " + entry.getKey() + "=" + entry.getValue());
+        }
+        
         
         // TODO check if serving API
         
